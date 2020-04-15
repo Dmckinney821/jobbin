594fde5234a1e435efd843dffbb881a84f6f7f82
Dan McKinney <3dhdaniel@gmail.com>
2020-04-15 17:28:40 -0400

    done for day

---
+++ server/models/rental.js
@@ -0,0 +1,47 @@
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const rentalSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        max: [128, 'No one got time for that many characters']
+    },
+    city: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    street: {
+        type: String,
+        required: true,
+        min: [4, 'make it longer']
+    },
+    category: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    bedrooms: {
+        type: Number,
+        required: true
+    },
+    shared: Boolean,
+    description: {
+        type: String,
+        required: true
+    },
+    dailyRate: Number,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('Rental', rentalSchema)
